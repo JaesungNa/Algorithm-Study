061713e0bd954a4ad963c271e40249d9bc922503
Jaesung Na <na-0003@hotmail.com>
2019-01-15 14:56:01 +0900

    bj2589 question

---
+++ src/bj2589/Main.java
@@ -0,0 +1,15 @@
+package bj2589;
+
+/**
+ * 백준 알고리즘
+ * #2589 보물섬
+ * 알고리즘 분류 : DFS/BFS 기본
+ *
+ * @author jaesungna
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
