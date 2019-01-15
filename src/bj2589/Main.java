b6b229b8cc387d60375eff61cd85506ae8b869f8
Jaesung Na <na-0003@hotmail.com>
2019-01-15 16:06:05 +0900

    get input

--- src/bj2589/Main.java
+++ src/bj2589/Main.java
@@ -1,5 +1,7 @@
 package bj2589;
 
+import java.util.Scanner;
+
 /**
  * 백준 알고리즘
  * #2589 보물섬
@@ -8,8 +10,21 @@ package bj2589;
  * @author jaesungna
  */
 public class Main {
+    private static String[][] MAP; // to store the given map
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+        MAP = new String[row][col];
+        sc.useDelimiter("");
+        sc.next();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                MAP[i][j] = sc.next();
+            }
+            sc.next();
+        }
 
     }
 }
