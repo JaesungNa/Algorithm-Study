b9b9f6b4b44db13533f5185759e437dec758b9d3
Jaesung Na <na-0003@hotmail.com>
2019-01-15 10:43:14 +0900

    bj2606 global variable declared

---
+++ src/bj2606/Main.java
@@ -0,0 +1,33 @@
+package bj2606;
+/**
+ * 백준 알고리즘
+ * #2606 바이러스
+ * 알고리즘 분류 : DFS/BFS 기본
+ *
+ * @author jaesungna
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    private static int numOfVertex; // number of vertex in given graph
+    private static int numOfEdges; // number of edges in given graph
+    private static int[][] adj; // adjacent matrix to store graph
+    private static boolean[] isVisited; // to store visited vertex in graph
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        numOfVertex = sc.nextInt();
+        numOfEdges = sc.nextInt();
+
+
+//        7
+//        6
+//        1 2
+//        2 3
+//        1 5
+//        5 2
+//        5 6
+//        4 7
+    }
+}
