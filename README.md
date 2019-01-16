77920a61b9f560309f6603664bc496e613f9ef5c
Jaesung Na <na-0003@hotmail.com>
2019-01-16 13:50:40 +0900

    bj7569 started

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Backjoon Questions
 1. '#1260 - DFS와 BFS : DFS/BFS 기본
 2. '#2606 - 바이러스 : DFS/BFS 기본
 3. '#2589 - 보물섬 : BFS(최단경로찾기)
+4. '#7569 - 토마토
---
+++ src/bj7569/Main.java
@@ -0,0 +1,16 @@
+package bj7569;
+
+
+/**
+ * 백준 알고리즘
+ * #7569 토마토
+ * 알고리즘 분류 :
+ *
+ * @author jaesungna
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
