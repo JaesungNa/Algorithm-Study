c730b1cdca7966d5c48d0292f10d7f1832058f92
Jaesung Na <na-0003@hotmail.com>
2019-01-16 14:10:50 +0900

    get input

--- src/bj7569/Main.java
+++ src/bj7569/Main.java
@@ -1,6 +1,8 @@
 package bj7569;
 
 
+import java.util.Scanner;
+
 /**
  * 백준 알고리즘
  * #7569 토마토
@@ -9,8 +11,22 @@ package bj7569;
  * @author jaesungna
  */
 public class Main {
+    private static int[][][] MAP;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt(); //width
+        int N = sc.nextInt(); //length
+        int H = sc.nextInt(); //height
+        MAP = new int[H][N][M];
+
+        for(int i=0; i<H;i++){
+            for(int j=0; j<N;j++){
+                for(int k=0; k<M;k++){
+                    MAP[i][j][k]=sc.nextInt();
+                }
+            }
+        }
 
     }
 }
