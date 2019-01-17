dbfb9a0e4d14b7cc60208e8273131774de515342
Jaesung Na <na-0003@hotmail.com>
2019-01-17 11:48:31 +0900

    skeleton

--- src/bj2151/Main.java
+++ src/bj2151/Main.java
@@ -1,5 +1,10 @@
 package bj2151;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /**
  * 백준 알고리즘
  * #2151 거울설치
@@ -10,4 +15,16 @@ package bj2151;
  */
 
 public class Main {
+    private static String[][] MAP;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        MAP = new String[N][N];
+
+        for(int l)
+
+
+    }
 }
