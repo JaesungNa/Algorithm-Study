b491398e0b6bf5d471b3f11381231a94d4ecfefe
Jaesung Na <na-0003@hotmail.com>
2019-01-17 12:56:52 +0900

    didn't survive

--- src/bj15802/Main.java
+++ src/bj15802/Main.java
@@ -1,6 +1,12 @@
 package bj15802;
 
-
+/**
+ * 백준 알고리즘
+ * #15802 타노스
+ * 알고리즘 분류 : 넌센스
+ *
+ * @author jaesungna
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println(0);
