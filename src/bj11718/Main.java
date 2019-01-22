987d3c2940af12504575f098946e1e38b364b218
Jaesung Na <na-0003@hotmail.com>
2019-01-22 17:17:45 +0900

    bj11718 done

--- src/bj11718/Main.java
+++ src/bj11718/Main.java
@@ -1,6 +1,14 @@
 package bj11718;
 
 import java.util.Scanner;
+/**
+ * 백준 알고리즘
+ * #11718 그대로 출력하기
+ * 알고리즘 분류 :
+ *
+ *
+ * @author jaesungna
+ */
 
 public class Main{
     public static void main(String[] args){
