2034dbe159ab529c5e9377d5b0e4fa752b6d06e3
Jaesung Na <na-0003@hotmail.com>
2019-02-08 15:40:48 +0900

    bubble/insertion/selection sort added

--- src/bj2750/Main.java
+++ src/bj2750/Main.java
@@ -1,7 +1,5 @@
 package bj2750;
 
-import com.sun.tools.javac.code.Attribute;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,7 +8,7 @@ import java.util.StringTokenizer;
 /**
  * 백준 알고리즘
  * #2750 수 정렬하기
- * 알고리즘 분류 : 선택정렬
+ * 알고리즘 분류 : 선택정렬/버블정렬/삽입정렬
  *
  * @author jaesungna
  */
@@ -28,7 +26,7 @@ public class Main {
             int temp = Integer.parseInt((new StringTokenizer(br.readLine())).nextToken());
             ARRAY[cnt] = temp;
         }
-        selectionSort();
+        insertionSort();
         print();
     }
 
@@ -36,17 +34,40 @@ public class Main {
         for (int i = 0; i < ARRAY.length; i++) {
             int minIndex = i;
             for (int j = i + 1; j < ARRAY.length; j++) {
-                if (ARRAY[j]<ARRAY[minIndex]){
-                    minIndex=j;
+                if (ARRAY[j] < ARRAY[minIndex]) {
+                    minIndex = j;
                 }
             }
             swap(i, minIndex);
         }
     }
-    public static void swap(int i, int maxIndex){
-        int temp =  ARRAY[i];
-        ARRAY[i]=ARRAY[maxIndex];
-        ARRAY[maxIndex]=temp;
+
+    public static void bubbleSort() {
+        for (int i = ARRAY.length; i > 0; i--) {
+            for (int j = 1; j < i; j++) {
+                if (ARRAY[j - 1] > ARRAY[j]) {
+                    swap(j, j - 1);
+                }
+            }
+        }
+    }
+
+    public static void insertionSort() {
+        for (int i = 1; i < ARRAY.length; i++) {
+            int curValue = ARRAY[i];
+            int pos = i;
+            while (pos > 0 && ARRAY[pos - 1] > curValue) {
+                ARRAY[pos] = ARRAY[pos - 1];
+                pos--;
+            }
+            ARRAY[pos] = curValue;
+        }
+    }
+
+    public static void swap(int i, int maxIndex) {
+        int temp = ARRAY[i];
+        ARRAY[i] = ARRAY[maxIndex];
+        ARRAY[maxIndex] = temp;
     }
 
     public static void print() {
