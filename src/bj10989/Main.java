393afeec5af788eac7c47063b1093462e67e61dc
Jaesung Na <na-0003@hotmail.com>
2019-02-11 23:44:21 +0900

    time out error fixed : use bw instead sout

--- src/bj10989/Main.java
+++ src/bj10989/Main.java
@@ -1,8 +1,6 @@
 package bj10989;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -24,11 +22,10 @@ public class Main {
         int R = 10001;
 
         cum = new int[R];
-        //Arrays.fill(cum, 0);
 
         int cnt = 0;
         int max = -1;
-        //읽음과 동시에 누적시키기
+
         while (cnt < N) {
             int curVal = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
             if (max < curVal) {
@@ -37,17 +34,18 @@ public class Main {
             cum[curVal] = ++cum[curVal];
             cnt++;
         }
-        //cum 원소 값들 배열 안에서 누적시키기
-//        for (int i = 1; i <= max; i++) {
-//            cum[i] = cum[i] + cum[i - 1];
-//        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         for (int i = 0; i <= max; i++) {
             if (cum[i] != 0) {
                 while (cum[i] > 0) {
-                    System.out.println(i);
+                    bw.write(i+"\n");
                     cum[i]--;
                 }
             }
         }
+        bw.flush();
+        bw.close();
     }
 }
