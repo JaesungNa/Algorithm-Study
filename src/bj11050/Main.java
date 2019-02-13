e5037e01f6063b333b6a30520d1321fb84694ef2
Jaesung Na <na-0003@hotmail.com>
2019-02-13 21:56:58 +0900

    Problem solved : #11050

--- src/bj11050/Main.java
+++ src/bj11050/Main.java
@@ -1,13 +1,39 @@
 package bj11050;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 /**
  * 백준 알고리즘
  * #11050 이항계수 1
- * 알고리즘 분류 :
+ * 알고리즘 분류 : recursion
  *
  * @author jaesungna
  */
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        bf.close();
 
+        int result = factorial(N) / (factorial(K) * factorial(N - K));
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(result + "");
+        bw.flush();
+        bw.close();
+    }
+
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return n;
+        }
+        return n * factorial(n - 1);
+    }
 }
