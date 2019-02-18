2072ee59368120913490e7ab0d35159accf80f8f
Jaesung Na <na-0003@hotmail.com>
2019-02-18 22:53:44 +0900

    Problem solved: #9251

--- src/bj9251/Main.java
+++ src/bj9251/Main.java
@@ -1,4 +1,10 @@
 package bj9251;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /**
  * 백준 알고리즘
  * #9251 LCS
@@ -7,7 +13,56 @@ package bj9251;
  * @author jaesungna
  */
 public class Main {
-    public static void main(String[] args){
+    private static char[] list1;
+    private static char[] list2;
+    private static int[][] cache;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        list1 = st.nextToken().toCharArray();
+
+        st = new StringTokenizer(br.readLine());
+        list2 = st.nextToken().toCharArray();
+        cache = new int[list1.length+1][list2.length+1];
+//        LCS1(0, 0, 0);
+        LCS2();
+//        print();
+        System.out.println(cache[list1.length][list2.length]);
+    }
+    public static void print(){
+        for(int i=0; i<cache.length;i++){
+            for(int j=0; j<cache[i].length;j++){
+                System.out.print(cache[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void LCS1(int i, int j, int nextCount) {
+        if (i >= cache.length - 1 || j >= cache[0].length) {
+            return;
+        }
+        if (list1[i] == list2[j]) {
+            if (nextCount < cache[i][j]) {
+                return;
+            }
+            cache[i][j] = ++nextCount;
+            LCS1(i + 1, j + 1, cache[i][j]);
+        } else {
+            LCS1(i + 1, j, nextCount);
+            LCS1(i, j + 1, nextCount);
+        }
+    }
+
+    public static void LCS2() {
+        for(int i=1;i<=list1.length;i++){
+            for(int j=1;j<=list2.length;j++){
+                if(list1[i-1] == list2[j-1]){
+                    cache[i][j]=cache[i-1][j-1]+1;
+                }else{
+                    cache[i][j]=Math.max(cache[i-1][j],cache[i][j-1]);
+                }
+            }
+        }
     }
 }
