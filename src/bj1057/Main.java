adf93bdb2b8a7810352e2bc5c0e02e5820b2bbda
Jaesung Na <na-0003@hotmail.com>
2019-02-23 19:55:15 +0900

    Problem solved: #1057

--- src/bj1057/Main.java
+++ src/bj1057/Main.java
@@ -1,5 +1,10 @@
 package bj1057;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /**
  * 백준 알고리즘
  * #1057 토너먼트
@@ -9,7 +14,24 @@ package bj1057;
  */
 
 public class Main {
-    public static void main(String[] args){
+    private static int K;
+    private static int I;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        I = Integer.parseInt(st.nextToken());
+        int count = 0;
+        while (K != I) {
+            K = K / 2 + K % 2;
+            I = I / 2 + I % 2;
+            count++;
+        }
+        System.out.println(count);
     }
+
+
 }
